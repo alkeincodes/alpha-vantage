56235fe0bd161c5ccec587c384e1d6a070ce5bf0
Alkein Villajos <aidukomusikero06@gmail.com>
2021-07-15 18:39:28 +0800

    added alert component

--- src/views/SiteTesting/Pages/PagesPerformance.vue
+++ src/views/SiteTesting/Pages/PagesPerformance.vue
@@ -18,6 +18,25 @@
           <data-card :data="data" />
         </b-col>
       </b-row>
+
+      <div class="data-table mt-4">
+        <b-alert
+          show
+          dismissible
+        >
+          <div class="alert-with-icon">
+            <icon-graduation-hat />
+            <span class="text-success"><strong>+236</strong> clicks from new keywords containing the phrase '<strong>compare</strong>'</span>
+          </div>
+        </b-alert>
+        <b-table
+          :items="tableData"
+          :fields="fields"
+          striped
+          sticky-header
+          hover
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -35,7 +54,40 @@ export default {
   computed: {
     dataCards () {
       return this.$store.getters['SiteTesting/dataCard']
+    },
+    tableData () {
+      return this.$store.getters['SiteTesting/tableData']
     }
+  },
+  data () {
+    return {
+      fields: [
+        {
+          key: 'date',
+          width: 180,
+          sortable: true
+        },
+        {
+          key: 'open',
+          sortable: true
+        },
+        {
+          key: 'high',
+          sortable: true
+        },
+        {
+          key: 'low',
+          sortable: true
+        },
+        {
+          key: 'close',
+          sortable: true
+        }
+      ]
+    }
+  },
+  created () {
+    this.$store.dispatch('SiteTesting/getApiData')
   }
 }
 </script>
